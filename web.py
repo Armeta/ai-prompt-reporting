526a0a190f98c84d6b263fed62685e4a2db39692
lancewahlert <lance.wahlert@armeta.com>
2023-07-26 18:03:44 -0500

    Updates to web.py

--- web.py
+++ web.py
@@ -1,83 +1,110 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+# data manipulation 
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+
+#data types
 import json
-import html
+
+# AI 
 import openai
 
-hostName = 'localhost'
-serverPort = 8080
+# Visualizations 
+import streamlit as st
+from PIL import Image
+import webbrowser
 
-
-f = open('Options.json','r')
-options = json.load(f)
-f.close()
-
-
-# OpenAI model
-if(options['model'] == 'text-embedding-ada-002'):
-    f = open('secrets.json','r')
-    secrets = json.load(f)
+# load options file and set up model
+def env_Setup():
+    # Open and collect options.json
+    f       = open('Options.json','r')
+    options = json.load(f)
     f.close()
-    openai.organization = secrets['organization']
-    openai.api_key = secrets['api_key']
-else:
-    model = SentenceTransformer(options['model'])
 
-opts = [option['url'] if option['type'] == 'url' else option['result'] for option in options['options']]
-opt_enc = [option['encoding'] for option in options['options']]
+    # OpenAI model
+    if(options['model'] == 'text-embedding-ada-002'):
+        f       = open('secrets.json','r')
+        secrets = json.load(f)
+        f.close()
+        openai.organization = secrets['organization']
+        openai.api_key      = secrets['api_key']
+    else:
+        model = SentenceTransformer(options['model'])
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        prompt = ''
-        if(self.path[0:4] == '/?q='):
-            prompt = self.path[4:].replace('+', ' ')
-            parts = prompt.split('%')
-            tmp = parts[0]
-            for part in parts[1:]:
-                tmp += chr(int(part[:2], 16)) + part[2:]
-            prompt = tmp
-            print(prompt)
+    #recieve options and their encodings and return    
+    opts    = [option['url'] if option['type'] == 'url' else option['result'] for option in options['options']]
+    opt_enc = [option['encoding'] for option in options['options']]
+    return options, model, opt_enc, opts
 
-        if(prompt != ''): # prompt is supplied, show result
-            encoding = None
-            if(options['model'] == 'text-embedding-ada-002'):
-                encoding = openai.Embedding.create(input = [prompt], model='text-embedding-ada-002')['data'][0]['embedding']
-            else:
-                encoding = model.encode(prompt)
-            
-            sim = cosine_similarity([encoding], opt_enc)
-            answer = opts[sim[0].tolist().index(max(sim[0]))]
+# sets up initial streamlit instance
+def page_Setup():
+    # tab icon
+    image = Image.open('.\\img\\icons\\armeta-icon.png')
+
+    # Page Config
+    st.set_page_config(
+        page_title="AI Turkey",
+        page_icon=image,
+        layout="wide",
+        initial_sidebar_state="expanded",
+        menu_items={
+            'About': "This is a webpage with a user input box where to input natural language and recieve real information along with links to a dashboard to help satisfy your query"
+        }
+    )
+
+    # Page Header/Subheader
+    st.title("AI Turkey")
+
+# run the prompt against the AI to recieve an answer
+def do_GET(prompt, options, model, opt_enc, opts):   
+    #init 
+    encoding = None
+    
+    # Encode prompt based off which model is being used
+    if(options['model'] == 'text-embedding-ada-002'):
+        encoding = openai.Embedding.create(input = [prompt], model='text-embedding-ada-002')['data'][0]['embedding']
+    else:
+        encoding = model.encode(prompt)
+    
+    # pick and return an answer based off options.json
+    sim = cosine_similarity([encoding], opt_enc)
+    answer = opts[sim[0].tolist().index(max(sim[0]))]
+    return answer, sim
+
+def main():
+    # gets mapping file and their encodings as well as meta data for the model being used
+    options, model, opt_enc, opts = env_Setup()
+
+    # sets up initial streamlit instance
+    page_Setup()
+    
+    # recieve prompt from user and draw a line to seperate results from question
+    st.subheader('What would you like to see?')
+    prompt = st.text_input('What would you like to see?', 'What was the total sales revenue yesterday?', label_visibility='collapsed')
+    st.divider() 
+
+    # run the prompt against the AI to recieve an answer
+    answer, sim = do_GET(prompt, options, model, opt_enc, opts)
+
+    # shows query results if any
+    with st.expander("Query results", expanded=True):  
+        if((answer[:5] != 'https') and (answer != '')):
             print('Similarity: %f, %s' % (max(sim[0]), answer))
-            self.wfile.write(bytes('<html><head>', 'utf-8'))
-            self.wfile.write(bytes('<title>Sample AI</title><style>body {font-size: 2em;}</style>', 'utf-8'))
-            if(answer[:5] == 'https'):
-                self.wfile.write(bytes('<meta http-equiv="Refresh" content="0; url=\''+answer+'\'" />', 'utf-8'))
-                self.wfile.write(bytes('</head><body>Redirecting to dashboard. Click <a href="'+answer+'">here</a> if not redirected.</body></html>', 'utf-8'))
-            else: # answer like SELECT
-                self.wfile.write(bytes('</head><body><p>Jim: '+prompt+'</p><p>AI: '+answer+'</p></body></html>', 'utf-8'))
-        else: # prompt == '', show prompt page
-            self.wfile.write(bytes('<html><head><title>Sample AI</title><style>body {font-size: 2em;} input {font-size: 1em;}</style></head>', 'utf-8'))
-            self.wfile.write(bytes('<body>', 'utf-8'))
-            #self.wfile.write(bytes('<a target="_blank" href="https://ip.armeta.com/demo/analytics/sales-opportunity?query=eyJmYW1pbHkiOjd9">Result</a>', 'utf-8'))
-            self.wfile.write(bytes('<form target="_blank" method="get" autocomplete="off">', 'utf-8'))
-            self.wfile.write(bytes('<label for="q">What would you like to see?</label><br>', 'utf-8'))
-            self.wfile.write(bytes('<input type="text" size="80" name="q" required autocomplete="false"><br>', 'utf-8'))
-            self.wfile.write(bytes('<input type="submit">', 'utf-8'))
-            self.wfile.write(bytes('</form>', 'utf-8'))
-            self.wfile.write(bytes('</body></html>', 'utf-8'))
+            st.write(answer)
+        else:
+            st.write("No query results")
+
+    # seperates dashboard results from query results
+    st.divider() 
+
+    # shows dashboards results if any
+    with st.expander("Dashboard results", expanded=True):        
+        if(answer[:5] == 'https'):
+            print('Similarity: %f, %s' % (max(sim[0]), answer))
+            url = answer
+            if st.button('Open Dashboard'):
+                webbrowser.open_new_tab(url)
+        else:            
+            st.write("No dashboard results")
 
 if __name__ == '__main__':        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print('Server started http://%s:%s' % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print('Server stopped.')
+    main()
