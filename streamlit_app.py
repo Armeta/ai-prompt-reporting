7b2d8cdc4222623a839c0d124323b83a56b27a76
lancewahlert <lance.wahlert@armeta.com>
2023-08-10 15:13:13 -0500

    UI updates. Gave options for filtering results

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,32 @@ from streamlit_option_menu import option_menu
 import time
 import streamlit as st
 
+# tab icon
+image = Image.open('src/media/armeta-icon.png')
+
+# Bot Avatar Icon
+with open('src/txt/armeta-icon_Base64Source.txt') as f:
+    BotAvatar = f.read()
+f.close()
+
+# User Avatar Icon
+with open('src/txt/usericon_Base64Source.txt') as f:
+    UserAvatar = f.read()
+f.close()
+
+# Page Config
+st.set_page_config(
+    page_title="Retail Analytics Digital Assistant",
+    page_icon=image,
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'About': "This is a webpage with a user input box where to input natural language and recieve real information along with links to a dashboard to help satisfy your query"
+    }
+)
+
 # load options file and set up model
+@st.cache_data()
 def env_Setup():
     # Open and collect options
     f            = open('src/json/Options.json','r')
@@ -50,57 +75,64 @@ def do_GET(prompt, model, dash_enc, dash_opts, query_enc, query_opts):
     query_answer = query_opts[sim[0].tolist().index(max(sim[0]))]
     return dash_answer, query_answer
 
-# tab icon
-image = Image.open('src/media/armeta-icon.png')
 
-# Bot Avatar Icon
-with open('src/txt/armeta-icon_Base64Source.txt') as f:
-    BotAvatar = f.read()
-f.close()
+def save_UserCache(i, content):
+    # set indice
+    name = 'messages' + str(i) 
+    st.session_state[name].append({"role": "user", "content": content})
 
-# User Avatar Icon
-with open('src/txt/usericon_Base64Source.txt') as f:
-    UserAvatar = f.read()
-f.close()
+def save_AssistantCache(i, content):
+    # set indice
+    name = 'messages' + str(i) 
+    st.session_state[name].append({"role": "assistant", "content": content})
 
-# Page Config
-st.set_page_config(
-    page_title="Retail Analytics Digital Assistant",
-    page_icon=image,
-    layout="wide",
-    initial_sidebar_state="collapsed",
-    menu_items={
-        'About': "This is a webpage with a user input box where to input natural language and recieve real information along with links to a dashboard to help satisfy your query"
-    }
-)
 
 # Open CSS file
 with open('src/css/style.css') as f:
    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 f.close()
-   
-if 'messages' not in st.session_state:
-    st.session_state['messages'] = []
 
 # Page Header/Subheader
 st.title("💬 arai") 
 with st.chat_message("assistant", avatar = BotAvatar):
     st.write("How can I help you?")
 
-def main():
+if 'sessionCount' not in st.session_state:
+        st.session_state['sessionCount'] = 0
 
-    # sidebar options
-    with st.sidebar:
-        if st.button("Reset chat"):            
-            with st.spinner("Loading..."):
-                del st.session_state['messages']
-                st.session_state['messages'] = []
-                st.success("Done!")
+def main():
 
     # gets mapping file and their encodings as well as meta data for the model being used
     model, dash_enc, dash_opts, query_enc, query_opts = env_Setup()
-  
-    # load old session cache
+    
+    # (re)-initialize current chat 
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = []
+
+    # sidebar options
+    with st.sidebar:
+            # caching for chats  
+            number = st.number_input('Insert chat index number', min_value=0, max_value=None, value=0, step=1)
+            # create new cache
+            name = 'messages' + str(number) 
+            
+            # initialize session cache    
+            if name not in st.session_state:
+                st.session_state[name] = []
+
+                # remove data from current session cache 
+                del st.session_state['messages']
+                
+                # reinitialize current session cache
+                st.session_state['messages'] = []                
+            else:                
+                # if the name exists then we just swap out the cache
+                st.session_state['messages'] = st.session_state[name]
+            
+            # Give filtering options for AI results
+            options = st.radio("What would you like to see?",('Both Dashboard and Query Results', 'Dashboards Only', 'Query Results Only'))
+
+    # load session cache or reset if none
     for message in st.session_state.messages:
         if(message["role"] == "user"):
             with st.chat_message("user", avatar = UserAvatar):
@@ -108,7 +140,7 @@ def main():
         else:       
             with st.chat_message("assistant", avatar = BotAvatar):
                 st.markdown(message["content"])
-    
+   
     # recieve prompt from user
     prompt = st.chat_input("Send a Message")                     
     if prompt : 
@@ -116,36 +148,28 @@ def main():
         with st.chat_message("user", avatar = UserAvatar):
             st.markdown(prompt)
 
-        query_answer = ''
-
-        # run the prompt against the AI to recieve an answer
+        # run the prompt against the AI to recieve an answer And Write to session cache for user
         dash_answer, query_answer = do_GET(prompt, model, dash_enc, dash_opts, query_enc, query_opts)        
-                   
-        # Write to session cache for user
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        # End chat - user
+        save_UserCache(number, prompt)               
         
         #Start chat - assistant
         with st.chat_message("assistant", avatar = BotAvatar):
             # Show query result 
-            if(query_answer != ''):
-                st.write(query_answer)
+            if(query_answer != '') and (options != 'Dashboards Only'):
+                # Write results + session cache for assistant
+                st.write(query_answer)                
+                save_AssistantCache(number, query_answer)
 
-                # Write session cache for assistant 
-                st.session_state.messages.append({"role": "assistant", "content": query_answer})                   
-            else:
-                st.write("No query results")
-
-                # Write session cache for assistant 
-                st.session_state.messages.append({"role": "assistant", "content": "No query results"})  
+            elif (options != 'Dashboards Only'):
+                # Write results + session cache for assistant 
+                st.write("No query results")               
+                save_AssistantCache(number, "No query results")
 
             # Show dashboard result  
-            if(dash_answer != ''):                  
-                url = dash_answer
-                st.markdown("Your query reminds me of this [dashboard.](%s)" % url)
-
-                # Write session cache for assistant 
-                st.session_state.messages.append({"role": "assistant", "content": "Your query reminds me of this [dashboard.](%s)" % url})                        
+            if(dash_answer != '') and (options != 'Query Results Only'): 
+                # Write results + session cache for assistant
+                st.markdown("Your query reminds me of this [dashboard.](%s)" % dash_answer)                
+                save_AssistantCache(number, "Your query reminds me of this [dashboard.](%s)" % dash_answer)
     # End chat - assistant
 image.close()
 
