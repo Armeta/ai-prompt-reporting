b5d859eaf1acb84b7d0358807d58a02f1db51e63
jonathan-white-armeta <140530762+jonathan-white-armeta@users.noreply.github.com>
2023-08-17 09:51:33 -0500

    Encode options in snowflake

---
+++ EncodeAllOptions.py
@@ -0,0 +1,37 @@
+from sentence_transformers import SentenceTransformer
+import json
+import struct
+import   sys
+from     snowflake.snowpark           import Session
+from     snowflake.snowpark.functions import col, to_timestamp
+from     snowflake.snowpark.types     import IntegerType, StringType, StructField, StructType, DateType,LongType,DoubleType
+
+# add src to system path
+sys.path.append('src')
+
+from lib import code_library
+
+connectionString = open('src/json/connection_details.json', "r")
+connectionString = json.loads(connectionString.read())
+session          = code_library.snowconnection(connectionString)    
+
+options_dash  = session.table("\"OptionsDashboard\"")
+options_query = session.table("\"OptionsQuery\"")
+#options_dash_test.show()
+
+# model selection
+model = SentenceTransformer('all-distilroberta-v1')
+
+# get descriptions, encode, and upload
+
+dash    = options_dash.select(['SK', 'DESC']).to_pandas().values.tolist()
+for row in dash:
+    enc = model.encode(row[1])
+    bin = ''.join([''.join(['%02x' % (b) for b in bytearray(struct.pack('d', d))]) for d in enc])
+    session.sql('UPDATE "MODEL"."OptionsDashboard" SET ENCODING=TO_BINARY(\''+bin+'\') WHERE SK='+str(row[0])+';').collect()
+
+query   = options_query.select(['SK', 'DESC']).to_pandas().values.tolist()
+for row in query:
+    enc = model.encode(row[1])
+    bin = ''.join([''.join(['%02x' % (b) for b in bytearray(struct.pack('d', d))]) for d in enc])
+    session.sql('UPDATE "MODEL"."OptionsQuery" SET ENCODING=TO_BINARY(\''+bin+'\') WHERE SK='+str(row[0])+';').collect()
