c27397734728716bebe4bb932619d28baacdf196
lancewahlert <lance.wahlert@armeta.com>
2023-08-30 16:10:53 -0500

    update ot feedback

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,8 +93,8 @@ def main():
 # ask user if reply was helpful
     #on =  toggle('Activate feature')
     if toggle(widget = 'checkbox', label='Give Feedback', value = False):
-        st.session_state.FeedbackText   = st.text_input("What could this answer be improved?", "... ")  
-        st.session_state.FeedbackRating = st.radio("Was this helpful?", ["✅", "❌"], label_visibility=st.session_state.visibility, disabled=st.session_state.disabled, horizontal=st.session_state.horizontal, index = 0)                
+        st.session_state.FeedbackRating = st.radio("Was this helpful?", ["✅", "❌"], label_visibility=st.session_state.visibility, disabled=st.session_state.disabled, horizontal=st.session_state.horizontal, index = 0) 
+        st.session_state.FeedbackText   = st.text_input("How could this answer be improved?", "... ")                        
         if (st.session_state.FeedbackRating == "❌") or (st.session_state.FeedbackText != "... "):
             test = code_library.get_LastPrompt(number) 
             code_library.write_Audit(session, test, st.session_state.FeedbackRating, st.session_state.FeedbackText)
