6d2d38331534b353990981bd7b10197f333a3af1
cmarmeta <chase.mcfarlane@armeta.com>
2023-09-03 14:45:06 -0500

    remove index column from st.dataframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,17 +19,17 @@ def main() -> None:
 
     requisition_id = st.text_input("Requisition ID")
     requisition = cl.get_requisition(session, requisition_id)
-    st.write(requisition)
+    st.dataframe(requisition, hide_index=True)
 
     st.markdown("### Base Nurse Table")
     if 'nurse_df' not in st.session_state:
         st.session_state['nurse_df'] = cl.get_nurses(session)
     nurse_df = pd.DataFrame(st.session_state['nurse_df'])
-    st.dataframe(nurse_df)
+    st.dataframe(nurse_df, hide_index=True)
 
     st.markdown("### Nurse Scoring Table")
     nurse_df = cl.score_nurses(nurse_df, requisition)
-    st.dataframe(nurse_df)
+    st.dataframe(nurse_df, hide_index=True)
 
 
 if __name__ == '__main__':
