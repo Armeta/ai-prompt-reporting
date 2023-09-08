e856b67ae3db744d8bce00f458005abc56105a2a
lancewahlert <lance.wahlert@armeta.com>
2023-09-08 11:14:41 -0500

    testing setting up cahching on all functions

--- src/lib/code_library.py
+++ src/lib/code_library.py
@@ -7,6 +7,7 @@ from snowflake.snowpark.functions import col
 from streamlit_extras.stylable_container import stylable_container
 
 # setup connection with snowflake
+@st.cache_resource
 def snow_session() -> None:
     return Session.builder.configs({
         'account': st.secrets['account'],
@@ -17,7 +18,6 @@ def snow_session() -> None:
         'database': st.secrets['database'],
         'schema': st.secrets['schema']
     }).create()
-
 
 @st.cache_data
 def get_requisition(_session: Session, requisition_id: int) -> pd.DataFrame:
@@ -40,18 +40,20 @@ def get_requisition(_session: Session, requisition_id: int) -> pd.DataFrame:
                            .where(req.needid == requisition_id)
                            .collect())
 
+@st.cache_data
 def get_nurses(_session: Session) -> pd.DataFrame:
     nurse_df = _session.table('NURSES')
     
     return pd.DataFrame(nurse_df.collect())
 
-
+@st.cache_data
 def get_nurse_discipline_specialty(_session: Session, requisition: pd.DataFrame) -> [pd.DataFrame, pd.DataFrame]:
     dis = _session.table('DISCIPLINE').filter(col('"DisciplineID"') == int(requisition['Need_DisciplineID'][0])).select(col('"NurseID"').as_('"DisciplineNurseID"')).distinct()
     spe = _session.table('SPECIALITY').filter(col('"SpecialtyId"') == int(requisition['Need_SpecialtyID'][0])).select(col('"NurseID"').as_('"SpecialtyNurseID"')).distinct()
 
     return pd.DataFrame(dis.collect()), pd.DataFrame(spe.collect())
 
+@st.cache_data
 def _score_licensure(nurse_df: pd.DataFrame, requisition: pd.DataFrame) -> pd.DataFrame:
     eNLC_states = [
         'AL', 'AZ', 'AR', 'CO', 'DE', 'FL', 'GA', 'ID', 'IN', 'IA', 'KS', 'KY',
@@ -65,34 +67,39 @@ def _score_licensure(nurse_df: pd.DataFrame, requisition: pd.DataFrame) -> pd.Da
     nurse_df['Score_License'] = nurse_df.apply(lambda row : 1 if (requisition_in_eNLC and row['State'] in eNLC_states) or (row['State'] == requisition_state) else 0, axis=1)
     return nurse_df
 
+@st.cache_data
 def _score_discipline(nurse_df: pd.DataFrame) -> pd.DataFrame:
 
     nurse_df['Score_Discipline'] = nurse_df.apply(lambda row : (1 if row['HasDiscipline'] else 0), axis=1)
     return nurse_df
 
+@st.cache_data
 def _score_specialty(nurse_df: pd.DataFrame) -> pd.DataFrame:
 
     nurse_df['Score_Specialty'] = nurse_df.apply(lambda row : (1 if row['HasSpecialty'] else 0), axis=1)
     return nurse_df
 
+@st.cache_data
 def _score_recency(nurse_df: pd.DataFrame) -> pd.DataFrame:
     today = np.datetime64('2023-08-03')
 
     nurse_df['Score_Recency'] = nurse_df.apply(lambda row : 0 if pd.isna(row['LastContractEnd_Datetime']) else max(0, 1-max(0, int((today - row['LastContractEnd_Datetime'])/np.timedelta64(1, 'D')))/365.0), axis=1)
     return nurse_df
 
-
+@st.cache_data
 def _score_enddate(nurse_df: pd.DataFrame) -> pd.DataFrame:
     today = np.datetime64('2023-08-03')
 
     nurse_df['Score_Enddate'] = nurse_df.apply(lambda row : 1 if pd.isna(row['LastContractEnd_Datetime']) else max(0, 1-max(0, int((row['LastContractEnd_Datetime'] - today)/np.timedelta64(1, 'D')))/35.0), axis=1)
     return nurse_df
 
+@st.cache_data
 def _score_experience(nurse_df: pd.DataFrame) -> pd.DataFrame:
 
     nurse_df['Score_Experience'] = nurse_df.apply(lambda row : (0 if pd.isna(row['YearsOfExperience']) else min(1, max(0, row['YearsOfExperience']-2)/8)), axis=1)
     return nurse_df
-
+    
+@st.cache_data
 def distance(lat1, lat2, lon1, lon2):
      
     # The math module contains a function named
@@ -115,6 +122,7 @@ def distance(lat1, lat2, lon1, lon2):
     # calculate the result
     return(c * r)
 
+@st.cache_data
 def _score_proximity(nurse_df: pd.DataFrame, requisition: pd.DataFrame) -> pd.DataFrame:
     requisition_lat= requisition['Facility_Lat'][0]
     requisition_long = requisition['Facility_Long'][0]
@@ -126,7 +134,7 @@ def _score_proximity(nurse_df: pd.DataFrame, requisition: pd.DataFrame) -> pd.Da
     #requisition_city = requisition['Facility_City'][0]
     #nurse_df['Score_Proximity'] = nurse_df.apply(lambda row : (1 if row['City'] == requisition_city and row['State'] == requisition_state else (0.5 if row['State'] == requisition_state else 0)), axis=1)
 
-
+@st.cache_data
 def score_nurses(nurse_df: pd.DataFrame, requisition: pd.DataFrame) -> pd.DataFrame:
     nurse_df = _score_licensure(nurse_df, requisition)
     nurse_df = _score_discipline(nurse_df)
@@ -149,6 +157,7 @@ def score_nurses(nurse_df: pd.DataFrame, requisition: pd.DataFrame) -> pd.DataFr
                 , axis=1)
     return nurse_df
 
+@st.cache_resource
 def env_Setup():
     # set page details
     st.set_page_config(
