e1a6a784edd71a4b55e7b4536394433b9c797f3b
lancewahlert <lance.wahlert@armeta.com>
2023-09-18 11:49:31 -0500

    added updates to requirements

--- src/lib/code_library.py
+++ src/lib/code_library.py
@@ -19,6 +19,8 @@ def snowconnection():
     connection_config = toml.load(f)
     f.close()
     session = Session.builder.configs(connection_config).create()
+    #session_details = session.create_dataframe([[session._session_id,session.sql("select current_user();").collect()[0][0],str(session.get_current_warehouse()).replace('"',''),str(session.get_current_role()).replace('"','')]], schema=["session_id","user_name","warehouse","role"])
+    #session_details.collect()
     return session
 
 def save_UserCache(i, content):
