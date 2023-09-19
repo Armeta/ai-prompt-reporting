7a9315fe97d37f61cf665ffb269e7c19588a63ca
lancewahlert <140530781+lancewahlert@users.noreply.github.com>
2023-09-19 11:31:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,57 +2,40 @@
 import streamlit as st
 from   PIL import Image
 import sys
-from streamlit_toggle import toggle
-# add src to system path
-sys.path.append('src')
-
+from streamlit_extras.switch_page_button import switch_page
 # custom functions
-from lib import code_library
+from src.lib import code_library
 import json
-# tab icon
-image = Image.open('src/media/armeta-icon.png')
-
-# Page Config
-st.set_page_config(
-    page_title="Retail Analytics Digital Assistant",
-    page_icon=image,
-    layout="wide",
-    initial_sidebar_state="collapsed",
-    menu_items={
-        'About': "This is a webpage with a user input box where to input natural language and recieve real information along with links to a dashboard to help satisfy your query"
-    }
-)
 
 
 def main():
 
     # Get connection string paramaters
-    connectionString = open('src/json/connection_details.json', "r")
-    connectionString = json.loads(connectionString.read())
-    session          = code_library.snowconnection(connectionString)    
+    session = code_library.snowconnection()   
     
-    # gets mapping file and their encodings as well as meta data for the model being used
-    model, dash_enc, dash_opts, query_enc, query_opts, BotAvatar, UserAvatar = code_library.env_Setup(session)
-    
-    # (re)-initialize current chat 
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = []
+    model, dash_enc, dash_opts, query_enc, query_opts, BotAvatar, UserAvatar  \
+    = code_library.env_Setup(session                                          
+                             , "Analytics Digital Assistant - Armeta POC"  
+                             , "wide"                                         
+                             , "collapsed"                                     
+                             , {'About': "This is a webpage with a user input box where to input natural language and recieve real information along with links to a dashboard to help satisfy your query"} 
+                             , './src/media/Title.png' 
+                            )
 
-    #manage feedback session state
-    if 'visibility' not in st.session_state:
-        st.session_state.visibility     = 'visible'
-        st.session_state.disabled       = True
-        st.session_state.horizontal     = True
-        st.session_state.FeedbackRating = ''
-        st.session_state.FeedbackText   = ''
+    with st.chat_message("assistant", avatar = BotAvatar):
+         st.write("How can I help you?")  
 
     # sidebar options
     with st.sidebar:
-         # caching for chat
+        # caching for chat
         number = code_library.manage_Cache()
 
-         # Give filtering options for AI results
+        # Give filtering options for AI results
         options = st.radio("What would you like to see?",('Both Dashboard and Query Results', 'Dashboards Only', 'Query Results Only'))
+
+        st.sidebar.header("Visualizations")
+        plot_options = ["Bar plot", "Scatter plot", "Histogram", "Box plot"]
+        selected_plot = st.sidebar.selectbox("Choose a plot type", plot_options)
 
     # load chat history 
     code_library.load_Cache(UserAvatar, BotAvatar)
@@ -95,20 +78,11 @@ def main():
                 st.markdown("Your query reminds me of this [dashboard.](%s)" % dash_answer)                
                 code_library.save_AssistantCache(number, "Your query reminds me of this [dashboard.](%s)" % dash_answer)
         # End chat - assistant
-        #st.experimental_rerun()
-# ask user if reply was helpful
-    #on =  toggle('Activate feature')
-    # if toggle(widget = 'checkbox', label='Give Feedback', value = False):
-    #     st.session_state.FeedbackRating = st.radio("Was this helpful?", ["✅", "❌"], label_visibility=st.session_state.visibility, disabled=st.session_state.disabled, horizontal=st.session_state.horizontal, index = 0) 
-    #     st.session_state.FeedbackText   = st.text_input("How could this answer be improved?", "... ", disabled=st.session_state.disabled)                        
-    #     if (st.session_state.FeedbackRating == "❌") or (st.session_state.FeedbackText != "... "):
-    #         test = code_library.get_LastPrompt(number) 
-    #         code_library.write_Audit(session, test, st.session_state.FeedbackRating, st.session_state.FeedbackText)
-    
- 
 
- 
-image.close()
+# ask user if reply was helpful
+    if st.button('Give Feedback'):
+        switch_page('Give Feedback')
+
 
 if __name__ == '__main__':  
     main()
